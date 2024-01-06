f2906885fc103b5c0d88325edc56de88035097d1
minseok128 <minseok128128@gmail.com>
2024-01-06 16:31:31 +0900

    Remove legacy: 기존의 방식 제거

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -27,25 +27,3 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	ft_strlcat(new + s1_len, s2, s1_len + s2_len + 1);
 	return (new);
 }
-
-char	*ft_strjoin_s(char const *s1, char const *s2)
-{
-	size_t	s1_len;
-	size_t	s2_len;
-	char	*new;
-
-	if (!s1)
-		s1 = ft_strdup(" ");
-	if (!s2)
-		s2 = ft_strdup(" ");
-	if (!s1 || !s2)
-		return (0);
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	new = ft_calloc(s1_len + s2_len + 1, sizeof(char));
-	if (!new)
-		exit(1);
-	ft_strlcpy(new, s1, s1_len + 1);
-	ft_strlcat(new + s1_len, s2, s1_len + s2_len + 1);
-	return (new);
-}
