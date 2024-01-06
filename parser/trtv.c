6c208bfc7d71a658f404675737112c7715511d81
minseok128 <minseok128128@gmail.com>
2024-01-06 16:32:12 +0900

    Add trtv_strjoin_s: trtv에서만 쓰이는 전용 strjoin 추가

--- parser/trtv.c
+++ parser/trtv.c
@@ -12,6 +12,23 @@
 
 #include "parser.h"
 
+char	*trtv_strjoin_s(char const *s1, char const *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*new;
+
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	new = ft_calloc(s1_len + s2_len + 1, sizeof(char));
+	if (!new)
+		exit(1);
+	ft_strlcpy(new, s1, s1_len + 1);
+	ft_strlcat(new + s1_len, s2, s1_len + s2_len + 1);
+	free(s1);
+	return (new);
+}
+
 int	trtv_dollar_sign(char *word, int now, t_envs *envsp)
 {
 	char	*key;
