86353658a020adabdec8abfd3919af60184d1f89
michang <minseok128128@gmail.com>
2024-01-07 21:08:06 +0900

    Update header: printf 사용하기 위해 추가

--- libft_s/libft_s.h
+++ libft_s/libft_s.h
@@ -12,6 +12,7 @@
 
 #ifndef LIBFT_S_H
 # define LIBFT_S_H
+# include <stdio.h>
 # include "../libft/libft.h"
 
 typedef struct s_vector {
@@ -25,4 +26,6 @@ char	*ft_substr_s(char const *s, unsigned int start, size_t len);
 void	vec_init(t_vector *v, int capacity);
 void	vec_push_back(t_vector *v, void *item);
 void	vec_free(t_vector *v);
+void	vec_print(t_vector *v);
+
 #endif
