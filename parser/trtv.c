67506095839ad2d58482ac1c5ea3bd888679b1ad
minseok128 <minseok128128@gmail.com>
2024-01-09 00:31:36 +0900

    Add trtv: make new function trtv_make_new_word_without_quotes

--- parser/trtv.c
+++ parser/trtv.c
@@ -12,36 +12,42 @@
 
 #include "parser.h"
 
-void	trtv_quotes_removal(t_vector *word_split)
+char	trtv_make_new_word_without_quotes(char *word)
 {
-	int		i;
-	char	*word;
-	char	*new_word;
 	int		now;
+	char	*new_word;
 	int		new_now;
 	int		in_squote;
 	int		in_dquote;
 
+	new_word = ft_calloc_s(sizeof(char), ft_strlen(word) + 1);
+	now = 0;
+	new_now = 0;
+	in_squote = 0;
+	in_dquote = 0;
+	while (word[now])
+	{
+		if (word[now] == '\"' && !in_squote)
+			in_dquote = !in_dquote;
+		else if (word[now] == '\'' && !in_dquote)
+			in_squote = !in_squote;
+		else
+			new_word[new_now++] = word[now];
+		now++;
+	}
+	free(word);
+	return (new_word);
+}
+
+void	trtv_quotes_removal(t_vector *word_split)
+{
+	int		i;
+	char	*new_word;
+
 	i = 0;
 	while (i < word_split->size)
 	{
-		word = word_split->items[i];
-		new_word = ft_calloc_s(sizeof(char), ft_strlen(word) + 1);
-		now = 0;
-		new_now = 0;
-		in_squote = 0;
-		in_dquote = 0;
-		while (word[now])
-		{
-			if (word[now] == '\"' && !in_squote)
-				in_dquote = !in_dquote;
-			else if (word[now] == '\'' && !in_dquote)
-				in_squote = !in_squote;
-			else
-				new_word[new_now++] = word[now];
-			now++;
-		}
-		free(word);
+		new_word = trtv_make_new_word_without_quotes(word_split->items[i]);
 		word_split->items[i] = new_word;
 		i++;
 	}
