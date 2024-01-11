39c417b300510b7778e988dad0f8cded294e5af0
minseok128 <minseok128128@gmail.com>
2024-01-11 13:25:28 +0900

    Update header: strdup_s

--- libft_s/libft_s.h
+++ libft_s/libft_s.h
@@ -24,6 +24,7 @@ typedef struct s_vector {
 int		ft_isspace(char c);
 char	*ft_substr_s(char const *s, unsigned int start, size_t len);
 char	*ft_itoa_s(int n);
+char	*ft_strdup_s(const char *s1);
 void	vec_init(t_vector *v, int capacity);
 void	vec_push_back(t_vector *v, void *item);
 void	vec_free(t_vector *v);
