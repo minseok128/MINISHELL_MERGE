b101a748a02a67e0c74f6879cdf9fe945810cd3a
minseok128 <minseok128128@gmail.com>
2024-01-11 18:19:29 +0900

    Update tk: header merge

--- parser/tk.c
+++ parser/tk.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "parser.h"
+#include "../minishell.h"
 
 static int	tk_print(t_token *tk)
 {
