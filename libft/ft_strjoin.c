6880eba256e8fe0d4619626ba864326fe6c1aad2
minseok128 <minseok128128@gmail.com>
2024-01-11 13:22:26 +0900

    Remove strjoin_s: legacy

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -16,6 +16,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	size_t	s1_len;
 	size_t	s2_len;
+	size_t	i;
 	char	*new;
 
 	s1_len = ft_strlen(s1);
@@ -23,6 +24,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	new = ft_calloc(s1_len + s2_len + 1, sizeof(char));
 	if (!new)
 		return (0);
+	i = 0;
 	ft_strlcpy(new, s1, s1_len + 1);
 	ft_strlcat(new + s1_len, s2, s1_len + s2_len + 1);
 	return (new);
