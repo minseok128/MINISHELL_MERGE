de89c845a179f7bccb8ce2ce2f07ffb1a1abfd97
minseok128 <minseok128128@gmail.com>
2024-01-12 01:30:39 +0900

    Update exit:  for list

--- btin/btin_exit.c
+++ btin/btin_exit.c
@@ -38,10 +38,8 @@ long long	btin_is_overflow(long long num, int n, long long minus)
 long long	btin_atoi(char *str)
 {
 	long long	minus;
-	long long	i;
 	long long	num;
 
-	i = 0;
 	num = 0;
 	minus = 1;
 	while (*str >= 9 && *str <= 13)
@@ -55,8 +53,6 @@ long long	btin_atoi(char *str)
 			return (-1);
 		num = num * 10 + *str - '0';
 		str++;
-		if (num > 0)
-			i++;
 	}
 	if (*str != '\0')
 		return (-1);
@@ -78,17 +74,18 @@ void	btin_exit(t_cmds *cmds, int fork_flag)
 	int	n;
 
 	printf("exit\n");
-	if (cmds->argv[1] == NULL)
+	if (cmds->argv.items[1] == NULL)
 		btin_out(1, 0, NULL);
 	else
 	{
-		n = btin_atoi(cmds->argv[1]);
+		n = btin_atoi(cmds->argv.items[1]);
 		if (n == -1)
 		{
-			printf("bash: exit: %s: numeric argument required\n", cmds->argv[1]);
+			printf("bash: exit: %s:", (char *)cmds->argv.items[1]);
+			printf(" numeric argument required\n");
 			btin_out(1, 255, NULL);
 		}
-		else if (cmds->argv[2] != NULL)
+		else if (cmds->argv.items[2] != NULL)
 			btin_out(fork_flag, 1, "bash: exit: too many arguments");
 		else
 			btin_out(1, (char)n, NULL);
