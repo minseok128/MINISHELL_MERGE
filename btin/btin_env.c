4ee96cf20f572455b6fac3f34d0c87914f3435cd
seonjo1 <tjdgus9684@gmail.com>
2024-01-17 18:34:13 +0900

    Update btin_env.c : 출력을 ft_putstr_fd 로 통일

--- btin/btin_env.c
+++ btin/btin_env.c
@@ -22,7 +22,12 @@ void	btin_env(t_envs *envsp, int fork_flag)
 	while (node != NULL)
 	{
 		if (node->value != NULL)
-			printf("%s=%s\n", node->key, node->value);
+		{
+			ft_putstr_fd(node->key, 1);
+			ft_putchar_fd('=', 1);
+			ft_putstr_fd(node->value, 1);
+			ft_putchar_fd('\n', 1);
+		}
 		node = node->next;
 	}
 	btin_out(fork_flag, 0, NULL);
