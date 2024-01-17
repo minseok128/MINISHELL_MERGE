c13a7d525e3eb2d9acc7be86c74777a0d9821256
seonjo1 <tjdgus9684@gmail.com>
2024-01-17 16:07:35 +0900

    Update ex_all_close : RD_APPEND로 잘못 입력한거 수정

--- execute/ex_cmd_loop.c
+++ execute/ex_cmd_loop.c
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 11:31:16 by seonjo            #+#    #+#             */
-/*   Updated: 2024/01/17 16:02:27 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/01/17 16:07:08 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	ex_all_close(t_cmds *cmdsp)
 
 	while (cmdsp != NULL)
 	{
-		if ((cmdsp->type & RD_APPEND) != 0)
+		if ((cmdsp->type & RD_HEREDOC) != 0)
 			if (unlink(cmdsp->in_file) == -1)
 				exit(1);
 		// ex_free_string_array(cmdsp->argv);
