aaebaba0476b5074c2f5e2c47bfb1b81fa7d2df1
seonjo1 <tjdgus9684@gmail.com>
2024-01-18 14:36:47 +0900

    Update btin_cd.c : 에러메시지 형식 변경

--- btin/btin_cd.c
+++ btin/btin_cd.c
@@ -30,7 +30,7 @@ void	btin_modify_pwd(t_envs *envsp, char *old_pwd, int fork_flag)
 		pwd = getcwd(NULL, 1024);
 		if (pwd == NULL)
 			btin_out(1, errno, btin_make_errmsg("minishell: ", \
-				"getcwd: ", strerror(errno)));
+				"getcwd", strerror(errno)));
 		free(env_pwd->value);
 		env_pwd->value = pwd;
 	}
@@ -46,16 +46,15 @@ void	btin_cd_error(int flag, char *dest, char *pwd, int fork_flag)
 		return ;
 	if (flag == 1)
 		errmsg = btin_make_errmsg("minishell: cd: ", dest, \
-			": No such file or directory");
+			"No such file or directory");
 	else if (flag == 2)
 		errmsg = btin_make_errmsg("minishell: cd: ", dest, \
-			": Not a directory");
+			"Not a directory");
 	else if (flag == 3)
 		errmsg = btin_make_errmsg("minishell: cd: ", dest, \
-			": Permission denied");
+			"Permission denied");
 	else if (flag == 4)
-		errmsg = btin_make_errmsg("minishell: cd: ", "HOME", \
-			" not set");
+		errmsg = btin_make_errmsg("minishell: ", "cd", "HOME not set");
 	if (dest != NULL)
 		free(dest);
 	if (pwd != NULL)
@@ -71,17 +70,17 @@ void	btin_move_to_dest(t_envs *envsp, char *dest, int fork_flag)
 	pwd = getcwd(NULL, 1024);
 	if (pwd == NULL)
 		btin_out(1, errno, btin_make_errmsg("minishell: ", \
-			"getcwd: ", strerror(errno)));
+			"getcwd", strerror(errno)));
 	if (stat(dest, &dir_stat) == -1)
 		btin_out(1, errno, btin_make_errmsg("minishell: ", \
-		"stat: ", strerror(errno)));
+		"stat", strerror(errno)));
 	else if (S_ISDIR(dir_stat.st_mode) != 1)
 		btin_cd_error(2, dest, pwd, fork_flag);
 	else if (access(dest, X_OK) != 0)
 		btin_cd_error(3, dest, pwd, fork_flag);
 	else if (chdir(dest) == -1)
 		btin_out(1, errno, btin_make_errmsg("minishell: ", \
-		"chdir: ", strerror(errno)));
+		"chdir", strerror(errno)));
 	else
 	{
 		free(dest);
