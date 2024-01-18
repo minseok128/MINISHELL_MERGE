5321f9983eedc6ca9e5806de1e3c85a5bd89799e
michang <minseok128128@gmail.com>
2024-01-18 16:22:59 +0900

    Update mktr: heredoc

--- parser/mktr.c
+++ parser/mktr.c
@@ -27,7 +27,7 @@ EBNF(by michang)
 
 // <command_part>	::= <word>
 //					| <redir>
-int	mktr_command_part(t_tr_node **head, t_token **tk_now)
+int	mktr_command_part(t_tr_node **head, t_token **tk_now, int *heredoc_signal)
 {
 	if (!((*tk_now)->type >= T_REDIR_S_L && (*tk_now)->type <= T_REDIR_D_R)
 		&& (*tk_now)->type != T_WORD)
@@ -36,30 +36,35 @@ int	mktr_command_part(t_tr_node **head, t_token **tk_now)
 	(*head)->tk = *tk_now;
 	if ((*tk_now)->type >= T_REDIR_S_L && (*tk_now)->type <= T_REDIR_D_R)
 	{
-		*tk_now = (*tk_now)->next;
-		if ((*tk_now)->type != T_WORD)
+		if ((*tk_now)->next->type != T_WORD)
 			return (1);
-		else
-			(*head)->tk->str = (*tk_now)->str;
+		if ((*tk_now)->type == T_REDIR_S_L)
+		{
+			// *heredoc_signal = seonjo_new_func(char **lmiter)
+			if (*heredoc_signal == 1)
+				return (1);
+		}
+		*tk_now = (*tk_now)->next;
+		(*head)->tk->str = (*tk_now)->str;
 	}
 	*tk_now = (*tk_now)->next;
 	return (0);
 }
 
 //<command>			::= <command_part> {<command_part>}
-int	mktr_command(t_tr_node **head, t_token **tk_now)
+int	mktr_command(t_tr_node **head, t_token **tk_now, int *heredoc_signal)
 {
 	t_tr_node	*next_node;
 
 	*head = mktr_alloc_s(TR_COMMAND, 0);
-	if (mktr_command_part(&((*head)->left), tk_now))
+	if (mktr_command_part(&((*head)->left), tk_now, heredoc_signal))
 		return (1);
 	while (*tk_now && ((*tk_now)->type == T_WORD \
 		|| ((*tk_now)->type >= T_REDIR_S_L && (*tk_now)->type <= T_REDIR_D_R)))
 	{
 		next_node = mktr_alloc_s(TR_COMMAND, 0);
 		next_node->left = *head;
-		if (mktr_command_part(&(next_node->right), tk_now))
+		if (mktr_command_part(&(next_node->right), tk_now, heredoc_signal))
 			return (1);
 		*head = next_node;
 	}
@@ -68,7 +73,7 @@ int	mktr_command(t_tr_node **head, t_token **tk_now)
 
 //<pipeline>		::= "(" <list> ")"
 // 					| <command> {"|" <command>}
-int	mktr_pipeline(t_tr_node **head, t_token **tk_now)
+int	mktr_pipeline(t_tr_node **head, t_token **tk_now, int *heredoc_signal)
 {
 	t_tr_node	*next_node;
 
@@ -76,21 +81,21 @@ int	mktr_pipeline(t_tr_node **head, t_token **tk_now)
 	if (*tk_now && (*tk_now)->type == T_PARENT_L)
 	{
 		*tk_now = (*tk_now)->next;
-		if (mktr_list(&((*head)->left), tk_now))
+		if (mktr_list(&((*head)->left), tk_now, heredoc_signal))
 			return (1);
 		if (*tk_now && (*tk_now)->type != T_PARENT_R)
 			return (1);
 		*tk_now = (*tk_now)->next;
 		return (0);
 	}
-	if (mktr_command(&((*head)->left), tk_now))
+	if (mktr_command(&((*head)->left), tk_now, heredoc_signal))
 		return (1);
 	while (*tk_now && (*tk_now)->type == T_PIPE)
 	{
 		*tk_now = (*tk_now)->next;
 		next_node = mktr_alloc_s(TR_PIPELINE, 0);
 		next_node->left = *head;
-		if (mktr_command(&(next_node->right), tk_now))
+		if (mktr_command(&(next_node->right), tk_now, heredoc_signal))
 			return (1);
 		*head = next_node;
 	}
@@ -98,12 +103,12 @@ int	mktr_pipeline(t_tr_node **head, t_token **tk_now)
 }
 
 // <list>			::= <pipeline> {("&&" | "||") <pipeline>}
-int	mktr_list(t_tr_node **head, t_token **tk_now)
+int	mktr_list(t_tr_node **head, t_token **tk_now, int *heredoc_signal)
 {
 	t_tr_node	*next_node;
 
 	*head = mktr_alloc_s(TR_LIST, 0);
-	if (mktr_pipeline(&((*head)->left), tk_now))
+	if (mktr_pipeline(&((*head)->left), tk_now, heredoc_signal))
 		return (1);
 	while (*tk_now && ((*tk_now)->type == T_AND || (*tk_now)->type == T_OR))
 	{
@@ -111,7 +116,7 @@ int	mktr_list(t_tr_node **head, t_token **tk_now)
 		*tk_now = (*tk_now)->next;
 		next_node = mktr_alloc_s(TR_LIST, 0);
 		next_node->left = *head;
-		if (mktr_pipeline(&(next_node->right), tk_now))
+		if (mktr_pipeline(&(next_node->right), tk_now, heredoc_signal))
 			return (1);
 		*head = next_node;
 	}
@@ -121,10 +126,17 @@ int	mktr_list(t_tr_node **head, t_token **tk_now)
 int	mktr_make_tree(t_token *tk_head, t_tr_node **root)
 {
 	t_token		*tk_now;
+	int			heredoc_signal;
 
+	heredoc_signal = 0;
 	tk_now = tk_head;
-	if (mktr_list(root, &tk_now) || tk_now->type != T_NEWLINE)
-		return (mktr_print_unexpected(tk_now->str));
+	if (mktr_list(root, &tk_now, &heredoc_signal))
+	{
+		if (heredoc_signal == 1)
+			return (1);
+		if (tk_now->type != T_NEWLINE)
+			return (mktr_print_unexpected(tk_now->str));
+	}
 	else
 		return (0);
 		// return (test_tr_print_tree(*root, "INIT TREE"));
