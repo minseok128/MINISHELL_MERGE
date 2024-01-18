33ebb5a3553a3365fbb2827deaaeda2c1bf1ffcb
michang <minseok128128@gmail.com>
2024-01-18 16:45:28 +0900

    Update header: 파라미터 수정

--- minishell.h
+++ minishell.h
@@ -114,10 +114,10 @@ t_token_type	tk_is_meta_char(char *str);
 int				mktr_print_unexpected(char *str);
 int				mktr_free_node(t_tr_node *node);
 t_tr_node		*mktr_alloc_s(t_tr_type bnf_type, t_token *tk);
-int				mktr_list(t_tr_node **head, t_token **tk_now, t_mktr_info *info);
-int				mktr_pipeline(t_tr_node **head, t_token **tk_now, t_mktr_info *info);
-int				mktr_command(t_tr_node **head, t_token **tk_now, t_mktr_info *info);
-int				mktr_command_part(t_tr_node **head, t_token **tk_now, t_mktr_info *info);
+int				mktr_list(t_tr_node **head, t_token **tk_now, int *heredoc_signal);
+int				mktr_pipeline(t_tr_node **head, t_token **tk_now, int *heredoc_signal);
+int				mktr_command(t_tr_node **head, t_token **tk_now, int *heredoc_signal);
+int				mktr_command_part(t_tr_node **head, t_token **tk_now, int *heredoc_signal);
 
 // trtv
 void			trtv_expansion_travel(t_tr_node *node, t_envs *envsp);
