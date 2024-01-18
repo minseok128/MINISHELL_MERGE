1af6eea429b427d0a3094fb1fcc365a0a8573b77
seonjo1 <tjdgus9684@gmail.com>
2024-01-18 14:47:18 +0900

    Update ex_handle_fd.c : 에러메시지 형식 변경

--- execute/ex_handle_fd.c
+++ execute/ex_handle_fd.c
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 17:07:48 by seonjo            #+#    #+#             */
-/*   Updated: 2024/01/17 18:53:19 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/01/18 14:19:20 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ex_dup_to(int from, int to)
 {
 	if (dup2(from, to) == -1)
 		btin_out(1, errno, btin_make_errmsg("minishell: ", \
-			"dup2: ", strerror(errno)));
+			"dup2", strerror(errno)));
 }
 
 void	ex_open_input_fd(t_cmds *cmdsp)
@@ -25,11 +25,11 @@ void	ex_open_input_fd(t_cmds *cmdsp)
 
 	if (access(cmdsp->in_file, F_OK) != 0)
 		btin_out(1, 1, btin_make_errmsg("minishell: ", cmdsp->in_file, \
-			": No such file or directory"));
+			"No such file or directory"));
 	in_fd = open(cmdsp->in_file, O_RDONLY);
 	if (in_fd == -1)
 		btin_out(1, 1, btin_make_errmsg("minishell: ", cmdsp->in_file, \
-			": Permission denied"));
+			strerror(errno)));
 	ex_dup_to(in_fd, 0);
 }
 
@@ -43,6 +43,6 @@ void	ex_open_output_fd(t_cmds *cmdsp)
 		out_fd = open(cmdsp->out_file, O_WRONLY);
 	if (out_fd == -1)
 		btin_out(1, 1, btin_make_errmsg("minishell: ", cmdsp->out_file, \
-			": Permission denied"));
+			strerror(errno)));
 	ex_dup_to(out_fd, 1);
 }
