1c53c3fca7825d80db144a4c22be566ae412fb97
seonjo1 <tjdgus9684@gmail.com>
2024-01-19 14:48:50 +0900

    Update btin_env : 기존 리다이렉션 처리 삭제

--- btin/btin_env.c
+++ btin/btin_env.c
@@ -12,29 +12,21 @@
 
 #include "../minishell.h"
 
-void	btin_env(t_cmds *cmds, t_envs *envsp, int fork_flag)
+void	btin_env(t_envs *envsp, int fork_flag)
 {
-	int		out_fd;
 	t_envs	*node;
 
-	if (envsp == NULL)
-		return ;
-	out_fd = btin_out_fd(cmds, fork_flag);
-	if (out_fd == -1)
-		return ;
 	node = envsp->next;
 	while (node != NULL)
 	{
 		if (node->value != NULL)
 		{
-			ft_putstr_fd(node->key, out_fd);
-			ft_putchar_fd('=', out_fd);
-			ft_putstr_fd(node->value, out_fd);
-			ft_putchar_fd('\n', out_fd);
+			ft_putstr_fd(node->key, 1);
+			ft_putchar_fd('=', 1);
+			ft_putstr_fd(node->value, 1);
+			ft_putchar_fd('\n', 1);
 		}
 		node = node->next;
 	}
-	if (out_fd != 1)
-		close(out_fd);
 	btin_out(fork_flag, 0, NULL);
 }
