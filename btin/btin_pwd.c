567ffa6c6f90baac839d090acad3acafe43fb262
seonjo1 <tjdgus9684@gmail.com>
2024-01-19 15:05:53 +0900

    Update btin_pwd : 리다이렉션 기능 삭제

--- btin/btin_pwd.c
+++ btin/btin_pwd.c
@@ -12,22 +12,16 @@
 
 #include "../minishell.h"
 
-void	btin_pwd(t_cmds *cmds, int fork_flag)
+void	btin_pwd(int fork_flag)
 {
-	int		out_fd;
 	char	*pwd;
 
-	out_fd = btin_out_fd(cmds, fork_flag);
-	if (out_fd == -1)
-		return ;
 	pwd = getcwd(NULL, 1024);
 	if (pwd == NULL)
 		btin_out(1, errno, btin_make_errmsg("minishell: ", \
 			"getcwd", strerror(errno)));
-	ft_putstr_fd(pwd, out_fd);
-	ft_putchar_fd('\n', out_fd);
+	ft_putstr_fd(pwd, 1);
+	ft_putchar_fd('\n', 1);
 	free(pwd);
-	if (out_fd != 1)
-		close(out_fd);
 	btin_out(fork_flag, 0, NULL);
 }
