e61d886c8835bc3244d819adfd277dba496f5de1
michang <minseok128128@gmail.com>
2024-01-19 19:45:00 +0900

    Update mktr : tree print test code 부활

--- parser/mktr.c
+++ parser/mktr.c
@@ -145,6 +145,6 @@ int	mktr_make_tree(t_token *tk_head, t_tr_node **root)
 		if (tk_now->type != T_NEWLINE)
 			return (mktr_print_unexpected(tk_now->str));
 	}
-	return (0);
-	// return (test_tr_print_tree(*root, "INIT TREE"));
+	// return (0);
+	return (test_tr_print_tree(*root, "INIT TREE"));
 }
