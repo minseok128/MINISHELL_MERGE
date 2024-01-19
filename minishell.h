5a9037f6f7e7541e37b05caaf268cd38b73689dc
seonjo1 <tjdgus9684@gmail.com>
2024-01-19 15:04:44 +0900

    Update Project : btin_export_print 함수 변경

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 14:46:51 by michang           #+#    #+#             */
-/*   Updated: 2024/01/19 14:46:30 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/01/19 15:04:10 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -151,7 +151,7 @@ void			btin_env(t_envs *envsp, int fork_flag);
 void			btin_exit(t_cmds *cmds, int fork_flag);
 void			btin_out(int fork_flag, int error_code, char *errmsg);
 int				btin_is_valid_identifier(char *str);
-void			btin_print_declare_env(t_cmds *cmds, t_envs *envsp, int fork_flag);
+void			btin_export_print(t_envs *envsp, int fork_flag);
 void			btin_free_key_and_value(char **key_and_value, char *key, char *value);
 t_envs			*btin_make_envsp_node(char **key_and_value);
 t_envs			*btin_make_envsp(char **envp);
