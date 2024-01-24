b0e06448d9ffb94aa3823261f6cb9e0978f56d19
seonjo1 <tjdgus9684@gmail.com>
2024-01-24 16:54:11 +0900

    Update trtv_redirection : 주석처리 제거

--- parser/trtv_redirection.c
+++ parser/trtv_redirection.c
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 17:11:47 by seonjo            #+#    #+#             */
-/*   Updated: 2024/01/18 17:16:29 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/01/24 16:50:21 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,16 @@
 int	trtv_redir_s_l(t_cmds *cmd, char *file)
 {
 	int	fd;
-	
-	// 1. 만약 heredoc이 열려있으면 파일 삭제
+
 	if ((cmd->type & RD_HEREDOC) != 0)
 		if (unlink(cmd->in_file) == -1)
 			btin_out(1, errno, btin_make_errmsg("minishell: ", \
 				"unlink", strerror(errno)));
-	// 2. 구조체에 in_file과 type 초기화
 	cmd->in_file = file;
 	cmd->type &= ~RD_HEREDOC;
-	// 3. 파일 오픈
 	fd = open(cmd->in_file, O_RDONLY);
-	// 4. 파일 오픈 실패시 구조체 만들기 stop
 	if (fd == -1)
-	{
 		return (1);
-		// 구조체 만들기 stop
-	}
-	// 5. 파일 오픈 성공시 바로 닫기
 	else
 		close(fd);
 	return (0);
@@ -41,19 +33,12 @@ int	trtv_redir_s_l(t_cmds *cmd, char *file)
 int	trtv_redir_s_r(t_cmds *cmd, char *file)
 {
 	int	fd;
-	
-	// 1. 구조체에 out_file과 type 초기화
+
 	cmd->out_file = file;
 	cmd->type &= ~RD_APPEND;
-	// 2. 파일 오픈
 	fd = open(cmd->out_file, O_CREAT | O_TRUNC | O_WRONLY, 0777);
-	// 3. 파일 오픈 실패시 구조체 만들기 stop
 	if (fd == -1)
-	{
 		return (1);
-		// 구조체 만들기 stop
-	}
-	// 4. 파일 오픈 성공시 바로 닫기
 	else
 		close(fd);
 	return (0);
@@ -63,23 +48,15 @@ int	trtv_redir_d_l(t_cmds *cmd, char *file)
 {
 	int		fd;
 
-	// 1. 만약 heredoc이 열려있으면 파일 삭제
 	if ((cmd->type & RD_HEREDOC) != 0)
 		if (unlink(cmd->in_file) == -1)
 			btin_out(1, errno, btin_make_errmsg("minishell: ", \
 				"unlink", strerror(errno)));
-	// 2. 구조체에 in_file과 type 초기화
 	cmd->in_file = file;
 	cmd->type |= RD_HEREDOC;
-	// 3. 파일 오픈
 	fd = open(cmd->in_file, O_RDONLY);
-	// 4. 파일 오픈 실패시 구조체 만들기 stop
 	if (fd == -1)
-	{
 		return (1);
-		// 구조체 만들기 stop
-	}
-	// 5. 파일 오픈 성공시 바로 닫기
 	else
 		close(fd);
 	return (0);
@@ -88,19 +65,12 @@ int	trtv_redir_d_l(t_cmds *cmd, char *file)
 int	trtv_redir_d_r(t_cmds *cmd, char *file)
 {
 	int	fd;
-	
-	// 1. 구조체에 out_file과 type 초기화
+
 	cmd->out_file = file;
 	cmd->type |= RD_APPEND;
-	// 2. 파일 오픈
 	fd = open(cmd->out_file, O_CREAT | O_APPEND | O_WRONLY, 0777);
-	// 3. 파일 오픈 실패시 구조체 만들기 stop
 	if (fd == -1)
-	{
 		return (1);
-		// 구조체 만들기 stop
-	}
-	// 4. 파일 오픈 성공시 바로 닫기
 	else
 		close(fd);
 	return (0);
