464a8163e249f342ca39d26e6183bf04a0e39cf6
seonjo1 <tjdgus9684@gmail.com>
2024-01-25 13:49:44 +0900

    Update ex_execute : 실행부 경로를 이용한 실행 오류케이스 구현

--- execute/ex_execute.c
+++ execute/ex_execute.c
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 17:12:59 by seonjo            #+#    #+#             */
-/*   Updated: 2024/01/25 11:23:07 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/01/25 13:45:46 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ char	*ex_search_path(char *cmd, t_envs *envsp, int i)
 	if (envsp == NULL)
 		btin_out(1, 127, btin_make_errmsg("minishell: ", cmd, \
 			"No such file or directory"));
-	envp_path = ft_split(envsp->value, ':');
+	envp_path = ft_split_s(envsp->value, ':');
 	while (envp_path[i] != NULL)
 	{
 		path = ex_strjoin_c(envp_path[i++], cmd, '/');
@@ -79,6 +79,51 @@ int	ex_is_directory(char *cmd)
 		return (1);
 }
 
+void	ex_path_execute(char *cmd)
+{
+	int		i;
+	char	**str;
+	char	*path;
+	char	*pre_path;
+
+	str = ft_split_s(cmd, '/');
+	path = ft_calloc_s(1, sizeof(char));
+	i = 0;
+	if (cmd[0] != '.' && cmd[0] != '/')
+	{
+		pre_path = path;
+		path = ft_strjoin_s("./", path);
+		free(path);
+	}
+	while (str[i] != '\0')
+	{
+		pre_path = path;
+		if (i == 0 && cmd[0] != '/')
+			path = ft_strjoin_s(path, str[i]);
+		else if (i == 0)
+			path = ex_strjoin_c(path, str[i], '/');
+		free(pre_path);
+		if (access(path, F_OK) == -1)
+			btin_out(1, 127, btin_make_errmsg("minisehll: ", cmd, \
+				"No such file or directory"));
+		if (str[i + 1] != '\0' && ex_is_directory(path) == 0)
+			btin_out(1, 126, btin_make_errmsg("minisehll: ", cmd, \
+				"Not a directory"));
+		else if (str[i + 1] == '\0' && ex_is_directory(path) == 1)
+			btin_out(1, 126, btin_make_errmsg("minisehll: ", cmd, \
+				"is a directory"));
+		if (access(path, X_OK) == -1)
+			btin_out(1, 126, btin_make_errmsg("minisehll: ", cmd, \
+				"Permission denied"));
+		i++;
+	}
+	free(path);
+	i = 0;
+	while (str[i] != NULL)
+		free(str[i++]);
+	free(str);
+}
+
 void	ex_execute(char **cmd, t_envs *envsp, char **envp)
 {
 	t_envs	*envsp_cp;
@@ -90,17 +135,10 @@ void	ex_execute(char **cmd, t_envs *envsp, char **envp)
 	else if (ft_strncmp(cmd[0], "..", 3) == 0 || cmd[0][0] == '\0')
 		btin_out(1, 127, btin_make_errmsg("minishell: ", cmd[0], \
 			"command not found"));
-	if (ex_is_directory(cmd[0]) == 1 && ex_is_path(cmd[0]) == 1)
-		btin_out(1, 126, btin_make_errmsg("minishell: ", cmd[0], \
-			"is a directory"));
+	if (ex_is_path(cmd[0]) == 1)
+		ex_path_execute(cmd[0]);
 	else if (ex_is_directory(cmd[0]) == 1 || access(cmd[0], F_OK) == -1)
-	{
-		if (ex_is_path(cmd[0]) == 1)
-			btin_out(1, 127, btin_make_errmsg("minisehll: ", cmd[0], \
-				"No such file or directory"));
-		else
-			cmd[0] = ex_search_path(cmd[0], envsp_cp, 0);
-	}
+		cmd[0] = ex_search_path(cmd[0], envsp_cp, 0);
 	execve(cmd[0], cmd, envp);
 	btin_out(1, errno, btin_make_errmsg("minishell: ", cmd[0], \
 			strerror(errno)));
