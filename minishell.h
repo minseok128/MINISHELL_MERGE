45e22f58fd2a52a0ad2e259eeafabc1f7ebe444e
michang <minseok128128@gmail.com>
2024-01-25 16:03:55 +0900

    Update Project : header에 함수 순서 변경

--- minishell.h
+++ minishell.h
@@ -135,22 +135,23 @@ int				mktr_heredoc_open_file(char **file_name);
 pid_t			mktr_heredoc_fork(int fd, char *limiter);
 
 // trtv
-char			*trtv_join_s(char *s1, char *s2);
-int				trtv_expansion(t_tr_node *node, t_envs *envsp);
-void			trtv_env_cmdp(char *word, char **e_w, t_envs *envsp);
-void			trtv_word_split(char *word, t_tr_node *node);
-void			trtv_quotes_removal(t_vector *word_split);
+int				trtv_list_travel(t_tr_node *node, t_envs *envsp);
 int				trtv_comd_part_travel(t_tr_node *node, t_cmds *cmd, \
 					t_envs *envsp);
 int				trtv_comd_travel(t_tr_node *node, t_cmds *cmd, t_envs *envsp);
 int				trtv_pipe_travel(t_tr_node *node, t_cmds *cmds_h, \
 					t_envs *envsp);
-int				trtv_list_travel(t_tr_node *node, t_envs *envsp);
+int				trtv_expansion(t_tr_node *node, t_envs *envsp);
+void			trtv_env_expand(char *word, char **e_w, t_envs *envsp);
+void			trtv_word_split(char *word, t_tr_node *node);
+void			trtv_quotes_removal(t_vector *word_split);
+void			trtv_wcard_expand(t_vector **word_split);
 int				trtv_redir_s_l(t_cmds *cmd, char *file);
 int				trtv_redir_s_r(t_cmds *cmd, char *file);
 int				trtv_redir_d_l(t_cmds *cmd, char *file);
 int				trtv_redir_d_r(t_cmds *cmd, char *file);
-void			trtv_wcard_expand(t_vector **word_split);
+char			*trtv_join_s(char *s1, char *s2);
+void			trtv_node_free(t_tr_node *node);
 
 // btin
 void			btin_pwd(int fork_flag);
