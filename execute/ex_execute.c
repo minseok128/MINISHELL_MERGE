8b4652bc0a25ee66c1109351535b95c1b25b1d60
seonjo1 <tjdgus9684@gmail.com>
2024-01-25 14:18:00 +0900

    Update ex_execute : ex_path_execute함수와 ex_file_name_execute 함수를 ex_execute_utils.c파일로 이전

--- execute/ex_execute.c
+++ execute/ex_execute.c
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 17:12:59 by seonjo            #+#    #+#             */
-/*   Updated: 2024/01/25 14:13:29 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/01/25 14:15:32 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,62 +79,6 @@ int	ex_is_directory(char *cmd)
 		return (1);
 }
 
-void	ex_path_execute(char *cmd)
-{
-	int		i;
-	char	**str;
-	char	*path;
-	char	*pre_path;
-
-	str = ft_split_s(cmd, '/');
-	path = ft_calloc_s(1, sizeof(char));
-	i = 0;
-	if (cmd[0] != '.' && cmd[0] != '/')
-	{
-		pre_path = path;
-		path = ft_strjoin_s("./", path);
-		free(path);
-	}
-	while (str[i] != NULL)
-	{
-		pre_path = path;
-		if (i == 0 && cmd[0] != '/')
-			path = ft_strjoin_s(pre_path, str[i]);
-		else
-			path = ex_strjoin_c(pre_path, str[i], '/');
-		free(pre_path);
-		if (access(path, F_OK) == -1)
-			btin_out(1, 127, btin_make_errmsg("minisehll: ", cmd, \
-				"No such file or directory"));
-		if (access(path, X_OK) == -1)
-			btin_out(1, 126, btin_make_errmsg("minisehll: ", cmd, \
-				"Permission denied"));
-		if (str[i + 1] != NULL && ex_is_directory(path) == 0)
-			btin_out(1, 126, btin_make_errmsg("minisehll: ", cmd, \
-				"Not a directory"));
-		else if (str[i + 1] == NULL && ex_is_directory(path) == 1)
-			btin_out(1, 126, btin_make_errmsg("minisehll: ", cmd, \
-				"is a directory"));
-		i++;
-	}
-	free(path);
-	i = 0;
-	while (str[i] != NULL)
-		free(str[i++]);
-	free(str);
-}
-
-char	*ex_file_name_execute(char *cmd, t_envs *envsp)
-{
-	cmd = ex_search_path(cmd, envsp->next, 0);
-	if (ex_is_directory(cmd) == 1)
-		btin_out(1, 127, btin_make_errmsg("minishell: ", cmd, \
-			"command not found"));
-	if (access(cmd, X_OK) == -1)
-		btin_out(1, 126, btin_make_errmsg("minisehll: ", cmd, \
-			"Permission denied"));
-	return (cmd);
-}
 
 void	ex_execute(char **cmd, t_envs *envsp, char **envp)
 {
