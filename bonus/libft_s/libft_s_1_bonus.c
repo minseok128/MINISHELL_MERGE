34dff9d952f4c801b1952c738ec335e5839726cf
seonjo1 <tjdgus9684@gmail.com>
2024-01-26 20:08:31 +0900

    Update libft_s_1_bonus : btin_out이 전부 fork_flag가 1이므로 eno에 NULL 넣어줌

--- bonus/libft_s/libft_s_1_bonus.c
+++ bonus/libft_s/libft_s_1_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft_s_1.c                                        :+:      :+:    :+:   */
+/*   libft_s_1_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 22:07:03 by michang           #+#    #+#             */
-/*   Updated: 2024/01/18 14:19:37 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/01/26 20:07:05 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*ft_substr_s(char const *s, unsigned int start, size_t len)
 	new = ft_substr(s, start, len);
 	if (!new)
 		btin_out(1, errno, btin_make_errmsg("minishell: ", \
-			"malloc", strerror(errno)));
+			"malloc", strerror(errno)), NULL);
 	return (new);
 }
 
@@ -35,7 +35,7 @@ char	*ft_itoa_s(int n)
 	new = ft_itoa(n);
 	if (!new)
 		btin_out(1, errno, btin_make_errmsg("minishell: ", \
-			"malloc", strerror(errno)));
+			"malloc", strerror(errno)), NULL);
 	return (new);
 }
 
@@ -46,7 +46,7 @@ char	*ft_strdup_s(const char *s1)
 	new = ft_strdup(s1);
 	if (!new)
 		btin_out(1, errno, btin_make_errmsg("minishell: ", \
-			"malloc", strerror(errno)));
+			"malloc", strerror(errno)), NULL);
 	return (new);
 }
 
@@ -57,6 +57,6 @@ void	*ft_calloc_s(size_t count, size_t size)
 	new = ft_calloc(count, size);
 	if (!new)
 		btin_out(1, errno, btin_make_errmsg("minishell: ", \
-			"malloc", strerror(errno)));
+			"malloc", strerror(errno)), NULL);
 	return (new);
 }
