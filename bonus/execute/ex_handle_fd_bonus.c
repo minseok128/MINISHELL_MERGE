ebef27efba9902da7a2d3137d762c124c51f9fe7
seonjo1 <tjdgus9684@gmail.com>
2024-01-26 20:05:50 +0900

    Update ex_handle_fd_bonus : eno 추가

--- bonus/execute/ex_handle_fd_bonus.c
+++ bonus/execute/ex_handle_fd_bonus.c
@@ -6,17 +6,17 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 17:07:48 by seonjo            #+#    #+#             */
-/*   Updated: 2024/01/26 19:05:37 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/01/26 19:44:19 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell_bonus.h"
 
-void	ex_dup_to(int from, int to)
+void	ex_dup_to(t_cmds *cmds, int from, int to)
 {
 	if (dup2(from, to) == -1)
 		btin_out(1, errno, btin_make_errmsg("minishell: ", \
-			"dup2", strerror(errno)));
+			"dup2", strerror(errno)), cmds->enop);
 }
 
 void	ex_open_input_fd(t_cmds *cmdsp)
@@ -25,12 +25,12 @@ void	ex_open_input_fd(t_cmds *cmdsp)
 
 	if (access(cmdsp->in_file, F_OK) != 0)
 		btin_out(1, 1, btin_make_errmsg("minishell: ", cmdsp->in_file, \
-			"No such file or directory"));
+			"No such file or directory"), cmdsp->enop);
 	in_fd = open(cmdsp->in_file, O_RDONLY);
 	if (in_fd == -1)
 		btin_out(1, 1, btin_make_errmsg("minishell: ", cmdsp->in_file, \
-			strerror(errno)));
-	ex_dup_to(in_fd, 0);
+			strerror(errno)), cmdsp->enop);
+	ex_dup_to(cmdsp, in_fd, 0);
 }
 
 void	ex_open_output_fd(t_cmds *cmdsp)
@@ -43,8 +43,8 @@ void	ex_open_output_fd(t_cmds *cmdsp)
 		out_fd = open(cmdsp->out_file, O_WRONLY);
 	if (out_fd == -1)
 		btin_out(1, 1, btin_make_errmsg("minishell: ", cmdsp->out_file, \
-			strerror(errno)));
-	ex_dup_to(out_fd, 1);
+			strerror(errno)), cmdsp->enop);
+	ex_dup_to(cmdsp, out_fd, 1);
 }
 
 int	ex_open_btin_output_fd(t_cmds *cmds)
@@ -63,7 +63,7 @@ int	ex_open_btin_output_fd(t_cmds *cmds)
 		return (-1);
 	}
 	std_out = dup(1);
-	ex_dup_to(out_fd, 1);
+	ex_dup_to(cmds, out_fd, 1);
 	close(out_fd);
 	return (std_out);
 }
@@ -87,7 +87,7 @@ int	ex_open_btin_input_fd(t_cmds *cmds)
 		return (-1);
 	}
 	std_in = dup(0);
-	ex_dup_to(in_fd, 0);
+	ex_dup_to(cmds, in_fd, 0);
 	close(in_fd);
 	return (std_in);
 }
