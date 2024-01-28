4b67b934cb89a5f91efd9ac7f0198e115708f2ec
michang <minseok128128@gmail.com>
2024-01-28 15:47:17 +0900

    Update main : parser 관련 함수 제거

--- bon/main_bonus.c
+++ bon/main_bonus.c
@@ -12,40 +12,6 @@
 
 #include "minishell_bonus.h"
 
-void	parser_info_init(int argc, char **argv, t_parser_info *p_info)
-{
-	p_info->eno = (argv - argv) + (argc - argc);
-	g_signal = &(p_info->eno);
-	ft_parser_info_zero(p_info);
-}
-
-void	parser_info_free(t_parser_info *p_info)
-{
-	t_token	*t_node;
-	int		i;
-
-	i = 0;
-	while (p_info->hdocs && i < p_info->hdocs->size)
-	{
-		unlink(p_info->hdocs->items[i]);
-		free(p_info->hdocs->items[i++]);
-	}
-	vec_free(p_info->hdocs);
-	free(p_info->hdocs);
-	free(p_info->line);
-	t_node = p_info->tk_head;
-	while (p_info->tk_head)
-	{
-		t_node = p_info->tk_head;
-		if (t_node->str)
-			free(t_node->str);
-		p_info->tk_head = p_info->tk_head->next;
-		free(t_node);
-	}
-	trtv_node_free(p_info->root);
-	ft_parser_info_zero(p_info);
-}
-
 int	main(int argc, char **argv, char **envp)
 {
 	t_envs			*envsp;
